f6f258f9b9a60f11cb79344dd9b0faff3f2fded3
Sentostalo <popsiclegt.aj@gmail.com>
2017-11-24 09:31:47 +1300

    Remove the echo command

--- cogs/public_commands.py
+++ cogs/public_commands.py
@@ -1,46 +1,43 @@
-'''
-Public commands, usable by anyone.
-'''
+import datetime
+import os
+import remy
+import sys
+import time
+import traceback
+import xmltools
 
 import discord
 from discord.ext import commands
-import time
-import datetime
-import remy
-import xmltools
-import sys
-import traceback
-import os
 from discord.ext.commands.cooldowns import BucketType
 
+
 class PublicCommandsCog:
-	def __init__(self,client):
-		self.client=client
+	def __init__(self, client):
+		self.client = client
 
-	@commands.command(name='echo',description='Says what you said back to you.')
-	@commands.cooldown(1, 10, BucketType.user)
-	async def do_echo(self,ctx, *message: str):
-		message=' '.join(message)
-		await ctx.send(message)
+	async def __local_check(self, ctx):
+		return not isinstance(ctx.channel, discord.DMChannel)
 
-	@commands.command(name='info',description='Gives the bot information.')
+	@commands.command(name='info', description='Gives the bot information.')
 	@commands.cooldown(1, 5, BucketType.user)
-	async def do_info(self,ctx):
+	async def do_info(self, ctx):
 		up_time = (datetime.datetime.strptime((time.ctime()), "%a %b %d %H:%M:%S %Y")) - (
 			datetime.datetime.strptime(ctx.bot.boot_time, "%a %b %d %H:%M:%S %Y"))
-		await ctx.message.channel.send(content=f'**Info**```{ctx.bot.user.name}: {ctx.bot.description}. Current uptime: {up_time}```')
+		await ctx.message.channel.send(
+			content=f'**Info**```{ctx.bot.user.name}: {ctx.bot.description}. Current uptime: {up_time}```')
 
 	@commands.command(name='avatar', description='Gets avatars of users mentioned.')
 	@commands.cooldown(1, 10, BucketType.user)
 	async def do_link(self, ctx):
-		urls=[]
-		if (ctx.message.mentions):
+		urls = []
+		if ctx.message.mentions:
 			for member in ctx.message.mentions:
 				urls.append(f'{member.name}: {member.avatar_url}')
-		urls='\n'.join(urls)
-		await ctx.send(f'**Avatar Urls**```{urls}```')
-
-
+		urls = '\n'.join(urls)
+		if urls:
+			await ctx.send(f'**Avatar Urls**```{urls}```')
+		else:
+			await ctx.send(f'**Avatar Urls**```No users specified.```')
 
 
 def setup(client):
