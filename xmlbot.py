290af18c1b15968cb02c3da85403ba9a69b192a8
Sentostalo <popsiclegt.aj@gmail.com>
2017-11-24 09:29:49 +1300

    Subclass commands.Bot

--- xmlbot.py
+++ xmlbot.py
@@ -11,27 +11,58 @@ valid_bot, bot_data = remy.get_bot('xml')
 
 
 def get_prefixes(discord_client, message):
-	prefixes = [bot_data['prefix'], '?']
+	prefixes = [bot_data['prefix'], '?', '>']
 	return commands.when_mentioned_or(*prefixes)(discord_client, message)
 
 
-client = commands.Bot(command_prefix=get_prefixes, description=bot_data['description'], game=discord.Game(name='Setup'))
+class XMLBot(commands.Bot):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 
 
-async def setup():
-	if not hasattr(client, 'boot_time'):
-		client.boot_time = (time.ctime())
-	if not hasattr(client, 'primary_channel'):
-		client.primary_channel = client.get_channel(bot_data['commands'])
-	if not hasattr(client, 'events_channel') and bot_data['events']:  # this is for xmlbot only
-		client.events_channel = client.get_channel(bot_data['events'])
-	if not hasattr(client, 'logs_channel'):
-		client.logs_channel = client.get_channel(bot_data['logs'])
-	if not hasattr(client, 'primary_guild'):
-		client.primary_guild = client.primary_channel.guild
-	if not hasattr(client, 'bot_data'):
-		client.bot_data = bot_data
+	async def setup(self):
+		if not hasattr(self, 'boot_time'):
+			self.boot_time = (time.ctime())
+		if not hasattr(self, 'primary_channel'):
+			self.primary_channel = self.get_channel(bot_data['commands'])
+		if not hasattr(self, 'events_channel') and 'events' in bot_data:  # this is for xmlbot only
+			self.events_channel = self.get_channel(bot_data['events'])
+		if not hasattr(self, 'logs_channel'):
+			self.logs_channel = self.get_channel(bot_data['logs'])
+		if not hasattr(self, 'primary_guild'):
+			self.primary_guild = self.primary_channel.guild
+		if not hasattr(self, 'bot_data'):
+			self.bot_data = bot_data
 
+
+	async def on_ready(self):
+		started = False
+		if not started:
+			await self.setup()
+			if __name__ == '__main__':
+				loadedcogs = []
+				failedcogs = []
+				for extension in bot_data['default_ext']:
+					try:
+						self.load_extension(extension)
+						print(f'Extension {extension} loaded.')
+						loadedcogs.append(extension)
+					except (SyntaxError, ModuleNotFoundError, NameError):
+						print(f'Failed to load extension {extension}.', file=sys.stderr)
+						if extension == 'cogs.core_cog':
+							print(f'Critical error, Core cog failed to load. Bot cannot run without this functionality.', file=sys.stderr)
+							raise SystemExit
+						traceback.print_exc()
+						failedcogs.append(extension)
+				loadedcogs = ' '.join(loadedcogs)
+				await self.cogs['CoreCog'].add_log(f'Connection established, {self.user.name} online. Primary channel is #{self.primary_channel} of {self.primary_guild}.\nCogs loaded: {loadedcogs}')
+				if len(failedcogs) != 0:
+					failedcogs = ' '.join(failedcogs)
+					await self.cogs['CoreCog'].add_log(f'The following cogs failed to load: {failedcogs}')
+			await client.change_presence(game=discord.Game(name='Testing'))
+			started=True
+
+client=XMLBot(command_prefix=get_prefixes, description=bot_data['description'], game=discord.Game(name='Setup'))
 
 def main():
 	if valid_bot:
@@ -39,32 +70,6 @@ def main():
 	else:
 		print('Invalid bot specified.')
 		raise SystemExit()
-
-
-@client.event
-async def on_ready():
-	started = False
-	if not started:
-		await setup()
-		if __name__ == '__main__':
-			loadedcogs = []
-			failedcogs = []
-			for extension in bot_data['default_ext']:
-				try:
-					client.load_extension(extension)
-					print(f'Extension {extension} loaded.')
-					loadedcogs.append(extension)
-				except Exception:
-					print(f'Failed to load extension {extension}.', file=sys.stderr)
-					traceback.print_exc()
-					failedcogs.append(extension)
-			loadedcogs = ' '.join(loadedcogs)
-			await client.cogs['DevCommandsCog'].add_log(f'Connection established, {client.user.name} online. Primary channel is #{client.primary_channel} of {client.primary_guild}.\nCogs loaded: {loadedcogs}')
-			if len(failedcogs) != 0:
-				failedcogs = ' '.join(failedcogs)
-				await client.cogs['DevCommandsCog'].add_log(f'The following cogs failed to load: {failedcogs}')
-		await client.change_presence(game=discord.Game(name='Testing'))
-		started=True
 
 
 main()
